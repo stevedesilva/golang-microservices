73e8910dce1c5db4fbac27af54a5d963bc4ffae4
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-06-15 21:14:52 +0100

    test

---
+++ src/api/domain/github/create_repo_test.go
@@ -0,0 +1,33 @@
+package github
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateRepoRequestAsJson(t *testing.T) {
+	request := CreateRepoRequest{
+		Name:        "golang introduction",
+		Description: "a golang introductory respository",
+		Homepage:    "https://github.com",
+		Private:     true,
+		HasIssues:   true,
+		HasProjects: true,
+		HasWiki:     true,
+	}
+
+	bytes, err := json.Marshal(request)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+
+    
+    var target CreateRepoRequest
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, target.Name, request.Name)
+    assert.EqualValues(t, target.Description, request.Description)
+}
