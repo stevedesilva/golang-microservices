48e4ccec6151bf395fbfad82a5e1fe67f96e6d89
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-06-16 09:47:30 +0100

    request

--- src/api/domain/github/create_repo_test.go
+++ src/api/domain/github/create_repo_test.go
@@ -8,6 +8,9 @@ import (
 )
 
 func TestCreateRepoRequestAsJson(t *testing.T) {
+     /*
+        Test Marshalling take input interface and attempt to create a json string ( obj -> json )
+    */
 	request := CreateRepoRequest{
 		Name:        "golang introduction",
 		Description: "a golang introductory respository",
@@ -16,18 +19,29 @@ func TestCreateRepoRequestAsJson(t *testing.T) {
 		HasIssues:   true,
 		HasProjects: true,
 		HasWiki:     true,
-	}
-
+    }
+   
 	bytes, err := json.Marshal(request)
 
-	assert.Nil(t, err)
-	assert.NotNil(t, bytes)
+	assert.Nil(t, err) // not error in marshalling
+	assert.NotNil(t, bytes) // we have json string
 
+
+    /*
+        Test Unmarshalling
+    */
+	var target CreateRepoRequest // struct to populate
+    err = json.Unmarshal(bytes, &target) // json string to target struct
     
-    var target CreateRepoRequest
-	err = json.Unmarshal(bytes, &target)
+    // compare to original request
 	assert.Nil(t, err)
-
 	assert.EqualValues(t, target.Name, request.Name)
     assert.EqualValues(t, target.Description, request.Description)
+    assert.EqualValues(t, target.Homepage, request.Homepage)
+    assert.True(t, target.Private)
+    assert.True(t, target.HasProjects)
+    assert.True(t, target.HasIssues)
+    assert.True(t, target.HasWiki)
 }
+
+// TODO test response
